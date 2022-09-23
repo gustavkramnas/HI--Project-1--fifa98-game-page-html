16271c8cd109aac0625ca668e151128fa6629097
Gustav <gustav@kramnas.se>
2022-09-23 14:21:25 +0200

    added javascript

--- script.js
+++ script.js
@@ -1,2 +1,80 @@
 // let nav = document.getElementById("nav");
 // nav.scrollLeft = 500;
+
+let spiderman = document.getElementById("spiderman");
+spiderman.addEventListener("click", popUpSpiderman);
+function popUpSpiderman() {
+  alert("Unfortunatly Spiderman is on vaccation :( Please come back later!");
+}
+
+let ninja = document.getElementById("ninja");
+ninja.addEventListener("click", popUpNinja);
+function popUpNinja() {
+  alert("Unfortunatly our Ninja is sick:( Please come back later!");
+}
+
+let combat = document.getElementById("combat");
+combat.addEventListener("click", popUpCombat);
+function popUpCombat() {
+  alert("Unfortunatly the fighters are having lunch :( Please come back soon!");
+}
+
+let donkeykong = document.getElementById("donkeykong");
+donkeykong.addEventListener("click", popUpDonkeyKong);
+function popUpDonkeyKong() {
+  alert(
+    "Ohhh, it looks like donkeykong having issues with Wreck-It Ralph :( Please come back!"
+  );
+}
+
+let gta = document.getElementById("gta");
+gta.addEventListener("click", popUpGta);
+function popUpGta() {
+  alert(
+    "Unfortunatly the city is under construction :( Please come back soon!"
+  );
+}
+
+let nba = document.getElementById("nba");
+nba.addEventListener("click", popUpNba);
+function popUpNba() {
+  alert(
+    "Unfortunatly our boys are having massage at the moment:( Please come back soon!"
+  );
+}
+
+let rage = document.getElementById("rage");
+rage.addEventListener("click", popUpRage);
+function popUpRage() {
+  alert("Unfortunatly this game is updating :( Please come back soon!");
+}
+
+let pacman = document.getElementById("pacman");
+pacman.addEventListener("click", popUpPacman);
+function popUpPacman() {
+  alert(
+    "Unfortunatly Pacman is recovering from a sqaure incident :( Please come back soon!"
+  );
+}
+
+let ristar = document.getElementById("ristar");
+ristar.addEventListener("click", popUpRistar);
+function popUpRistar() {
+  alert("Unfortunatly ristar broke his one arm :( Please come back soon!");
+}
+
+let sonic = document.getElementById("sonic");
+sonic.addEventListener("click", popUpSonic);
+function popUpSonic() {
+  alert(
+    "Unfortunatly Sonic lost his speed due to a ankle injury :( Please come back soon!"
+  );
+}
+
+let supermario = document.getElementById("supermario");
+supermario.addEventListener("click", popUpSupermario);
+function popUpSupermario() {
+  alert(
+    "Unfortunatly supermario having a pizzaparty :( Please come back after his food coma!"
+  );
+}
